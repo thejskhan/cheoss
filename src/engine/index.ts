4c52db4d4c33b50002f3e4157631206fc3f06e3a
thejskhan <thejargalsaikhan@gmail.com>
2023-11-17 19:04:43 +0800

    modular class per pieces

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,4 +1,4 @@
-import { CELL_COLOR, CELL_SIZE } from '../render/constants'
+import { CELL_COLOR, CELL_SIZE, TEXT_OFFSET } from '../render/constants'
 import { RANKS, FILES, IBoard } from './constants'
 import type { TColor, TPosition, TSquare } from './types'
 
@@ -18,30 +18,81 @@ import type { TColor, TPosition, TSquare } from './types'
 // A1, B1, C1...
 
 export class Piece {
+    name: string
     position: TPosition
     color: TColor
-    constructor(position: TPosition, color: TColor) {
+    image: HTMLImageElement
+    x: number
+    y: number
+    possibleMoves: TPosition[]
+    constructor(position: TPosition, name: string, x: number, y: number) {
         this.position = position
-        this.color = color
+        this.color = name.toLowerCase() === name ? 'b' : 'w'
+        this.name = name
+        this.image = new Image()
+        this.x = x
+        this.y = y
+        this.possibleMoves = []
+    }
+
+    draw(c: CanvasRenderingContext2D) {
+        c.drawImage(this.image, this.x, this.y, CELL_SIZE, CELL_SIZE)
     }
 }
 
 export class Pawn extends Piece {
-    asset: string
-    possibleMoves: TPosition[]
-    constructor(position: TPosition, color: TColor) {
-        super(position, color)
-        this.asset = `${color}p`
-        this.possibleMoves = []
+    constructor(position: TPosition, name: string, x: number, y: number) {
+        super(position, name, x, y)
+        this.image = new Image()
+        this.image.src = `/assets/${this.color}p.svg`
+    }
+}
+
+export class Knight extends Piece {
+    constructor(position: TPosition, name: string, x: number, y: number) {
+        super(position, name, x, y)
+        this.image = new Image()
+        this.image.src = `/assets/${this.color}n.svg`
+    }
+}
+
+export class Bishop extends Piece {
+    constructor(position: TPosition, name: string, x: number, y: number) {
+        super(position, name, x, y)
+        this.image = new Image()
+        this.image.src = `/assets/${this.color}b.svg`
+    }
+}
+
+export class Rook extends Piece {
+    constructor(position: TPosition, name: string, x: number, y: number) {
+        super(position, name, x, y)
+        this.image = new Image()
+        this.image.src = `/assets/${this.color}r.svg`
+    }
+}
+
+export class Queen extends Piece {
+    constructor(position: TPosition, name: string, x: number, y: number) {
+        super(position, name, x, y)
+        this.image = new Image()
+        this.image.src = `/assets/${this.color}q.svg`
+    }
+}
+
+export class King extends Piece {
+    constructor(position: TPosition, name: string, x: number, y: number) {
+        super(position, name, x, y)
+        this.image = new Image()
+        this.image.src = `/assets/${this.color}k.svg`
     }
 }
 
 export class Square {
     position: TPosition
     cellColor: TColor
-    square: TSquare
-    piece: string | undefined
-    image: HTMLImageElement | undefined
+    name: TSquare
+    piece: Piece | undefined
     x: number
     y: number
     constructor(
@@ -53,16 +104,35 @@ export class Square {
     ) {
         this.position = position
         this.cellColor = cellColor
-        this.square = `${position[0]}${position[1]}`
-        this.piece = piece
-        this.image = piece ? new Image() : undefined
+        this.name = `${position[0]}${position[1]}`
         this.x = x
         this.y = y
 
-        if (this.image) {
-            this.image.src = `/assets/${
-                this.piece.toLowerCase() === piece ? 'b' : 'w'
-            }${piece.toLowerCase()}.svg`
+        switch (piece) {
+            case 'p':
+            case 'P':
+                this.piece = new Pawn(this.position, piece, this.x, this.y)
+                break
+            case 'n':
+            case 'N':
+                this.piece = new Knight(this.position, piece, this.x, this.y)
+                break
+            case 'b':
+            case 'B':
+                this.piece = new Bishop(this.position, piece, this.x, this.y)
+                break
+            case 'r':
+            case 'R':
+                this.piece = new Rook(this.position, piece, this.x, this.y)
+                break
+            case 'q':
+            case 'Q':
+                this.piece = new Queen(this.position, piece, this.x, this.y)
+                break
+            case 'k':
+            case 'K':
+                this.piece = new King(this.position, piece, this.x, this.y)
+                break
         }
     }
 
@@ -71,8 +141,10 @@ export class Square {
             this.cellColor === 'w' ? CELL_COLOR.light : CELL_COLOR.dark
         c.fillRect(this.x, this.y, CELL_SIZE, CELL_SIZE)
 
-        if (this.image) {
-            c.drawImage(this.image!, this.x, this.y, CELL_SIZE, CELL_SIZE)
+        c.fillText(this.name, this.x + TEXT_OFFSET, this.y - TEXT_OFFSET)
+
+        if (this.piece) {
+            this.piece.draw(c)
         }
     }
 }
